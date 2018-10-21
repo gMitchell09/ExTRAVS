de3b11da38144c65efeb34639d5ebf457e5f58d1
Whitney Cosby <wsc0002@gmail.com>
2018-10-21 11:11:46 -0500

    Updated to check if waypoint is in the viewport

--- Assets/WaypointHUD.cs
+++ Assets/WaypointHUD.cs
@@ -7,16 +7,35 @@ public class WaypointHUD : MonoBehaviour {
     private GameObject[] m_waypoints;
     private Camera m_mainCamera;
 
+    [SerializeField]
+    public float[] ForwardDistances;
+
+    [SerializeField]
+    public Vector3[] WTF;
+
     void OnGUI() {
+        ForwardDistances = new float[m_waypoints.Length];
+        WTF = new Vector3[m_waypoints.Length];
+        int i = 0;
         foreach (GameObject wp in m_waypoints) {
-            float distance = Vector3.Distance(transform.position, wp.transform.position);
-            GUI.Label(new Rect(Camera.main.WorldToViewportPoint(wp.transform.position).x, 101, 100, 20), distance.ToString() + "m");
+            // Check if dot product of forward vector and vector for camera to way point is negative
+            Vector3 cameraToPoint = wp.transform.position.normalized - transform.position.normalized;
+            float forwardDotDistance = Vector3.Dot(transform.forward.normalized, wp.transform.position.normalized);
+            ForwardDistances[i++] = forwardDotDistance;
+            // Check if way point is visible, output > 0; else it's not visible 
+            if (forwardDotDistance >= 0) {
+                float distance = Vector3.Distance(transform.position, wp.transform.position);
+                Vector3 wpVectorView = m_mainCamera.WorldToScreenPoint(wp.transform.position);
+                WTF[i-1] = wpVectorView;
+                GUI.color = Color.black;
+                GUI.Label(new Rect(wpVectorView.x, wpVectorView.y, 100, 100), distance.ToString("F2") + "m");
+            }
         }
     }
 	// Use this for initialization
 	void Start () {
         m_waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
-        m_mainCamera = Camera.main;
+        m_mainCamera = GetComponent<Camera>();
 	}
 	
 	// Update is called once per frame
