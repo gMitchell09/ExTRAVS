26f507f9c934aac9599ed8d3bda2db0a418f81ec
George Mitchell <gpv0001@uah.edu>
2018-10-21 00:10:20 -0500

    Physics Updates, Waypoint updates

---
+++ Assets/SimpleCarController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleCarController : MonoBehaviour {
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    public void FixedUpdate() {
+        float motor = maxMotorTorque * Input.GetAxis("VerticalKey");
+        float steering = maxSteeringAngle * Input.GetAxis("HorizontalKey");
+
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+
+        if (Input.GetKeyDown("Jump")) {
+            Quaternion rot = transform.rotation;
+            rot.eulerAngles.Set(rot.x, rot.y, 0);
+            transform.rotation = rot;
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
---
+++ Assets/WaypointHUD.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaypointHUD : MonoBehaviour {
+    private GameObject[] m_waypoints;
+    private Camera m_mainCamera;
+
+    void OnGUI() {
+        foreach (GameObject wp in m_waypoints) {
+            float distance = Vector3.Distance(transform.position, wp.transform.position);
+            GUI.Label(new Rect(Camera.main.WorldToViewportPoint(wp.transform.position).x, 101, 100, 20), distance.ToString() + "m");
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        m_waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        m_mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
